56cf59d4a13be254446abb59b74ca5da5bac7e6b
Jimmy <jiawei.zhang95@outlook.com>
2026-03-15 17:38:14 +0800

    chore(quick-2): wire release signing to debug keystore in build.gradle.kts
    - Add signingConfigs.release block pointing to ~/.android/debug.keystore
    - Reference signingConfig in release buildType

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,15 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("${System.getProperty("user.home")}/.android/debug.keystore")
+            storePassword = "android"
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -25,6 +34,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
             )
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 
